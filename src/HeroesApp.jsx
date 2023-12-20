5d1f06730ebab007663a656fb133ea15f4969454
sebastian461 <sebastiano-461@hotmail.com>
2023-12-20 14:52:24 -0500

    Context y Reducer
    Se creó un contexto con un provider para que la información fluya a través de la aplicación, y un reducer para manejar el estado y las acciones que van a fluir

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./ui/components/Navbar";
+import { AuthProvider } from "./auth/context";
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <div className="container" id="detail">
         <Outlet />
       </div>
-    </>
+    </AuthProvider>
   );
 };
---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,13 @@
+import { useReducer } from "react";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+
+export const AuthProvider = ({ children }) => {
+  const initialState = {
+    logged: false,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+};
